7a8e50a989e1b4c0c2506191e821801b7d92acbc
“Thrisha <tkopula@ucsc.edu>
2022-04-19 23:17:24 -0700

    changed green UI

--- src/scenes/Play.js
+++ src/scenes/Play.js
@@ -10,7 +10,7 @@ class Play extends Phaser.Scene {
         this.mainBuilding = this.add.tileSprite(0, 0, 640, 480, 'mainBuilding').setOrigin(0, 0);
         this.road = this.add.tileSprite(0, 0, 640, 480, 'road').setOrigin(0, 0);
         // green UI background
-        this.add.rectangle(0, borderUISize + borderPadding, game.config.width, borderUISize * 2, 0x00FF00).setOrigin(0, 0);
+        this.add.rectangle(0, borderUISize + borderPadding, game.config.width, borderUISize * 2, 0x55b3b4).setOrigin(0, 0);
         // white borders
         this.add.rectangle(0, 0, game.config.width, borderUISize, 0xFFFFFF).setOrigin(0, 0);
         this.add.rectangle(0, game.config.height - borderUISize, game.config.width, borderUISize, 0xFFFFFF).setOrigin(0, 0);
